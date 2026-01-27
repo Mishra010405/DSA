53bb23c64cd9624fdf70883554076116817bc49b
Shivam Mishra <mshivam2062@gmail.com>
2026-01-27 19:31:20 +0530

    2D Array

--- sorting.java
+++ sorting.java
@@ -205,36 +205,36 @@
 
 
 // Bubble sort  in Deceiding otder...
-import java.util.*;
+// import java.util.*;
 
-public class JavaBasics {
+// public class JavaBasics {
 
-    public static void BubbleSort(int num[]) {
+//     public static void BubbleSort(int num[]) {
         
-        for (int i = 0; i < num.length - 1; i++) {
-            for (int j = 0; j < num.length - 1 - i; j++) {
-                if (num[j] < num[j + 1]) {
-                    int temp = num[j];
-                    num[j] = num[j + 1];
-                    num[j + 1] = temp;
-                }
-            }
-        }
-    }    
-    public static void PrintArray(int num[]) {
-        for (int i = 0; i < num.length; i++) {
-            System.out.print(num[i] + " ");
-        }
-        System.out.println();
-    }
+//         for (int i = 0; i < num.length - 1; i++) {
+//             for (int j = 0; j < num.length - 1 - i; j++) {
+//                 if (num[j] < num[j + 1]) {
+//                     int temp = num[j];
+//                     num[j] = num[j + 1];
+//                     num[j + 1] = temp;
+//                 }
+//             }
+//         }
+//     }    
+//     public static void PrintArray(int num[]) {
+//         for (int i = 0; i < num.length; i++) {
+//             System.out.print(num[i] + " ");
+//         }
+//         System.out.println();
+//     }
 
-    public static void main(String args[]) {
-        int num[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
+//     public static void main(String args[]) {
+//         int num[] = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
 
-        BubbleSort(num);
-        PrintArray(num);
-    }
-}
+//         BubbleSort(num);
+//         PrintArray(num);
+//     }
+// }
 
 
 
