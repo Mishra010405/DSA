f15f712e0f162fb986503f16df47ff87128142dc
Shivam Mishra <mshivam2062@gmail.com>
2026-01-27 11:06:56 +0530

    Counting Sort

--- sorting.java
+++ sorting.java
@@ -149,14 +149,56 @@
 
 // Convertion from Array to arraylist..
 
+// import java.util.*;
+
+// public class JavaBasics {
+//     public static void main(String args[]) {
+//         Integer arr[] = {2,3,4,5,6,86,6};
+
+//         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(arr));
+//         Collections.sort(list);
+//         System.out.println(list);
+//     }
+// }
+
+// COunting Sort ....
+
 import java.util.*;
 
 public class JavaBasics {
-    public static void main(String args[]) {
-        Integer arr[] = {2,3,4,5,6,86,6};
 
-        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(arr));
-        Collections.sort(list);
-        System.out.println(list);
+    public static void CountingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+
+        // Find largest element
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        // Count array
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        // Sorting
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    
+    public static void main(String args[]) {
+        int arr[] = {5, 4, 1, 3, 2};
+
+        CountingSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
