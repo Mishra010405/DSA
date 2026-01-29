e45563243d692622a062fc70d48ca307a9ec7612
Shivam Mishra <mshivam2062@gmail.com>
2026-01-29 22:50:47 +0530

    SOme of the Basic String Questions

--- CodeString.java
+++ CodeString.java
@@ -68,26 +68,67 @@
 
 // Check if it is a Polindrme...
 
+// public class JavaBasics {
+
+//     public static boolean Polindrme(String name) {
+//         int n = name.length();
+        
+//         for(int i = 0; i < n / 2; i++) {
+//             if(name.charAt(i) != name.charAt(n - i - 1)) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+//     public static void main(String[] args) {
+//         String name = "CarraC";
+
+//         if(Polindrme(name)) {
+//             System.out.println("So it is the palindrome");
+//         } else {
+//             System.out.println("So it is not the palindrome");
+//         }
+//     }
+// }
+
+
+// Get Shortest Path...
 public class JavaBasics {
 
-    public static boolean Polindrme(String name) {
-        int n = name.length();
-        
-        for(int i = 0; i < n / 2; i++) {
-            if(name.charAt(i) != name.charAt(n - i - 1)) {
-                return false;
+    public static double getShortestPath(String path) {
+        int x = 0;
+        int y = 0;
+
+        for(int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+
+            // South
+            if(dir == 'S') {
+                y--;
+            }
+            // North
+            else if(dir == 'N') {
+                y++;
+            }
+            // West
+            else if(dir == 'W') {
+                x--;
+            }
+            // East
+            else if(dir == 'E') {
+                x++;
             }
         }
-        return true;
+
+        int X2 = x * x;
+        int Y2 = y * y;
+
+        return Math.sqrt(X2 + Y2);
     }
 
     public static void main(String[] args) {
-        String name = "CarraC";
-
-        if(Polindrme(name)) {
-            System.out.println("So it is the palindrome");
-        } else {
-            System.out.println("So it is not the palindrome");
-        }
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
     }
 }
