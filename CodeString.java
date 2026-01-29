f7783be128e5b3494383bd5f296ed71258fadfc2
Shivam Mishra <mshivam2062@gmail.com>
2026-01-29 20:57:14 +0530

    SOme of the Basic String Questions

--- CodeString.java
+++ CodeString.java
@@ -1,9 +1,25 @@
-import java.util.*;
+// import java.util.*;
 
-public class JavaBasics{
-    public static void main(String args[]) {
-        Scanner Sc = new Scanner(System.in);
-        String name = Sc.nextLine();
-        System.out.println(name);
+// public class JavaBasics{
+//     public static void main(String args[]) {
+//         Scanner Sc = new Scanner(System.in);
+//         String name = Sc.nextLine();
+//         System.out.println(name);
+//     }
+// }
+
+// String as a Function..
+
+public class JavaBasics {
+
+    public static void NamePrint(String Firstname, String LastName) {
+        System.out.println(Firstname+" "+LastName);
+        System.out.println(Firstname.charAt(0));
     }
+
+    public static void main(String[] args) {
+        String Firstname = "Shivam";
+        String LastName = "Mishra";
+        NamePrint(Firstname, LastName);
+    } 
 }
