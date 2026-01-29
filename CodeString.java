27673aff83e5f18f04a901c9d28af87ec703263c
Shivam Mishra <mshivam2062@gmail.com>
2026-01-29 20:57:56 +0530

    SOme of the Basic String Questions

--- CodeString.java
+++ CodeString.java
@@ -10,16 +10,35 @@
 
 // String as a Function..
 
-public class JavaBasics {
+// public class JavaBasics {
 
-    public static void NamePrint(String Firstname, String LastName) {
-        System.out.println(Firstname+" "+LastName);
-        System.out.println(Firstname.charAt(0));
+//     public static void NamePrint(String Firstname, String LastName) {
+//         System.out.println(Firstname+" "+LastName);
+//         System.out.println(Firstname.charAt(0));
+//     }
+
+//     public static void main(String[] args) {
+//         String Firstname = "Shivam";
+//         String LastName = "Mishra";
+//         NamePrint(Firstname, LastName);
+//     } 
+// }
+
+
+// Create the Spacing Between The Latters...
+// import java.util.*;
+
+public class JavaBasics{
+    public static void SpaceBet(String full_name){
+        for(int i=0 ; i<full_name.length() ;i++) {
+            System.out.println(full_name.charAt(i)+"  ");
+        }
+
     }
-
-    public static void main(String[] args) {
-        String Firstname = "Shivam";
-        String LastName = "Mishra";
-        NamePrint(Firstname, LastName);
-    } 
+    public static void main(String args[]) {
+        String Name = "Shivam";
+        String Last = "Mishra";
+        String full_name = Name+Last;
+        SpaceBet(full_name);
+    }
 }
