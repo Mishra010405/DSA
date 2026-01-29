49e4874616d32a6fc6103c0124ececa38fc9a74b
Shivam Mishra <mshivam2062@gmail.com>
2026-01-29 20:59:03 +0530

    SOme of the Basic String Questions

--- CodeString.java
+++ CodeString.java
@@ -48,19 +48,46 @@
 
 // import java.util.*;
 
-public class JavaBasics{
-    public static void SpaceBet(String full_name){
-        for(int i=0 ; i<full_name.length() ;i++) {
-            if(full_name.charAt(i) == 'r'){
-                System.out.println(i+"  ");
+// public class JavaBasics{
+//     public static void SpaceBet(String full_name){
+//         for(int i=0 ; i<full_name.length() ;i++) {
+//             if(full_name.charAt(i) == 'r'){
+//                 System.out.println(i+"  ");
+//             }
+//         }
+
+//     }
+//     public static void main(String args[]) {
+//         String Name = "Shivam";
+//         String Last = "Mishra";
+//         String full_name = Name+Last;
+//         SpaceBet(full_name);
+//     }
+// }
+
+
+// Check if it is a Polindrme...
+
+public class JavaBasics {
+
+    public static boolean Polindrme(String name) {
+        int n = name.length();
+        
+        for(int i = 0; i < n / 2; i++) {
+            if(name.charAt(i) != name.charAt(n - i - 1)) {
+                return false;
             }
         }
-
+        return true;
     }
-    public static void main(String args[]) {
-        String Name = "Shivam";
-        String Last = "Mishra";
-        String full_name = Name+Last;
-        SpaceBet(full_name);
+
+    public static void main(String[] args) {
+        String name = "CarraC";
+
+        if(Polindrme(name)) {
+            System.out.println("So it is the palindrome");
+        } else {
+            System.out.println("So it is not the palindrome");
+        }
     }
 }
