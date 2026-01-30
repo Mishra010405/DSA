2946ba115dab2f63f898e1604d3789b543cc0e70
Shivam Mishra <mshivam2062@gmail.com>
2026-01-30 11:39:30 +0530

    SOme of the Basic String Questions

--- CodeString.java
+++ CodeString.java
@@ -94,41 +94,44 @@
 
 
 // Get Shortest Path...
-public class JavaBasics {
+// public class JavaBasics {
 
-    public static double getShortestPath(String path) {
-        int x = 0;
-        int y = 0;
+//     public static double getShortestPath(String path) {
+//         int x = 0;
+//         int y = 0;
 
-        for(int i = 0; i < path.length(); i++) {
-            char dir = path.charAt(i);
+//         for(int i = 0; i < path.length(); i++) {
+//             char dir = path.charAt(i);
 
-            // South
-            if(dir == 'S') {
-                y--;
-            }
-            // North
-            else if(dir == 'N') {
-                y++;
-            }
-            // West
-            else if(dir == 'W') {
-                x--;
-            }
-            // East
-            else if(dir == 'E') {
-                x++;
-            }
-        }
+//             // South
+//             if(dir == 'S') {
+//                 y--;
+//             }
+//             // North
+//             else if(dir == 'N') {
+//                 y++;
+//             }
+//             // West
+//             else if(dir == 'W') {
+//                 x--;
+//             }
+//             // East
+//             else if(dir == 'E') {
+//                 x++;
+//             }
+//         }
 
-        int X2 = x * x;
-        int Y2 = y * y;
+//         int X2 = x * x;
+//         int Y2 = y * y;
 
-        return Math.sqrt(X2 + Y2);
-    }
+//         return Math.sqrt(X2 + Y2);
+//     }
 
-    public static void main(String[] args) {
-        String path = "WNEENESENNN";
-        System.out.println(getShortestPath(path));
-    }
-}
+//     public static void main(String[] args) {
+//         String path = "WNEENESENNN";
+//         System.out.println(getShortestPath(path));
+//     }
+// }
+
+
+// String Comperistion...
