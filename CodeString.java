67a2389f8503d5d12b0e4e3cfb49557ceafe8d95
Shivam Mishra <mshivam2062@gmail.com>
2026-02-03 10:43:50 +0530

    All the process on the update process

--- CodeString.java
+++ CodeString.java
@@ -402,28 +402,25 @@
 // }
 
 public class JavaBasics {
-    public static void countVowels(String str) {
-        int vowels = 0;
-        int consonants = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-
-            if (ch >= 'a' && ch <= 'z') {   
-                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+    public static void CountString(String str) {
+        int vowels=0 ; 
+        int Consonants=0;
+        for(int i=0 ; i<str.length() ; i++) {
+            char ch  = Character.toLowerCase(str.charAt(i));
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
                     vowels++;
                 } else {
-                    consonants++;
+                    Consonants++;
                 }
-            }
         }
+        System.out.println("The Vowles Are "+ vowels);
+        System.out.println("The COnsonents Are "+ Consonants );
 
-        System.out.println("The vowels are: " + vowels);
-        System.out.println("The consonants are: " + consonants);
+
     }
 
     public static void main(String args[]) {
-        String str = "abcdef";
-        countVowels(str);
+        String str = "addsgbfbfbfb";
+        CountString(str);
     }
 }
