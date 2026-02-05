3147aaa21a35d2389869425d66a4812a12ecc517
Shivam Mishra <mshivam2062@gmail.com>
2026-02-05 18:39:17 +0530

    Bit Manipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -13,24 +13,49 @@
 
 // Conversion of the Binary 1's into Compliments...
 
-public class JavaBasics {
-    public static String onesComp(String bin) {
-        String result= "";
-        for(int i=0 ; i<bin.length() ; i++) {
-            if(bin.charAt(i) == '0') {
-                result += 1;
-            }
-            else {
-                result += 0;
-            }
-        }
+// public class JavaBasics {
+//     public static String onesComp(String bin) {
+//         String result= "";
+//         for(int i=0 ; i<bin.length() ; i++) {
+//             if(bin.charAt(i) == '0') {
+//                 result += 1;
+//             }
+//             else {
+//                 result += 0;
+//             }
+//         }
         
-        return result;
+//         return result;
+//     }
+
+//     public static void main(String args []) {
+//         String bin = "10101";
+//         System.out.println(onesComp(bin));
+
+//     }
+// }
+
+
+// Check if the number is Odd or Even....
+
+import java.util.*;
+
+public class JavaBasics {
+
+    public static void Oddeven(int num) {
+        if (num % 2 == 0) {
+            System.out.println("The number is Even");
+        } else {
+            System.out.println("The number is Odd");
+        }
     }
 
-    public static void main(String args []) {
-        String bin = "10101";
-        System.out.println(onesComp(bin));
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        Oddeven(num);
     }
 }
