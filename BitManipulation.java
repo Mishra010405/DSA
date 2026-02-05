7a0c9e22cdef5798dc932fd3ec1db8cf68d8fa32
Shivam Mishra <mshivam2062@gmail.com>
2026-02-05 18:24:41 +0530

    Bit Manipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,11 +1,36 @@
+// public class JavaBasics {
+//     public static void main(String args[]) {
+
+//         int num = 5;
+
+//         int onesComp = ~num;
+
+//         System.out.println("Number: " + num);
+//         System.out.println("1's Complement: " + ~5);
+//     }
+// }
+
+
+// Conversion of the Binary 1's into Compliments...
+
 public class JavaBasics {
-    public static void main(String args[]) {
+    public static String onesComp(String bin) {
+        String result= "";
+        for(int i=0 ; i<bin.length() ; i++) {
+            if(bin.charAt(i) == '0') {
+                result += 1;
+            }
+            else {
+                result += 0;
+            }
+        }
+        
+        return result;
+    }
 
-        int num = 5;
+    public static void main(String args []) {
+        String bin = "10101";
+        System.out.println(onesComp(bin));
 
-        int onesComp = ~num;
-
-        System.out.println("Number: " + num);
-        System.out.println("1's Complement: " + ~5);
     }
 }
