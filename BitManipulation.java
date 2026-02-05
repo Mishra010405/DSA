b268e608438d7d7d341982ae10aa798cdbfebb3c
Shivam Mishra <mshivam2062@gmail.com>
2026-02-05 23:32:44 +0530

    Bit Manipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -175,18 +175,22 @@
 
 // Count Set bits in a number...
 
-public class JavaBasics {
-    public static int CountsetBits(int n) {
-        int count=0 ; 
-        while(n>0) {
-            if((n&1) != 0) {
-                count++;
-            }
-            nn>>1;
-        }
+// public class JavaBasics {
+//     public static int CountsetBits(int n) {
+//         int count=0 ; 
+//         while(n>0) {
+//             if((n&1) != 0) {
+//                 count++;
+//             }
+//             nn>>1;
+//         }
 
-    }
-    public static void main(String args[]) {
-        System.out.println(CountsetBits(13));
-    }
-}
+//     }
+//     public static void main(String args[]) {
+//         System.out.println(CountsetBits(13));
+//     }
+// }
+
+
+// Fast Exponential.....
+
