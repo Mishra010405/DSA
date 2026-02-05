3c8263730d9ddc4f81935bd5a1a0d751d39a4d13
Shivam Mishra <mshivam2062@gmail.com>
2026-02-05 18:19:14 +0530

    Bit Manipulation

---
+++ BitManipulation.java
@@ -0,0 +1,11 @@
+public class JavaBasics {
+    public static void main(String args[]) {
+
+        int num = 5;
+
+        int onesComp = ~num;
+
+        System.out.println("Number: " + num);
+        System.out.println("1's Complement: " + ~5);
+    }
+}
