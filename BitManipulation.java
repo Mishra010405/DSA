ef471b0e6cb1753b90db60fcc80bb8082df61fbb
Shivam Mishra <mshivam2062@gmail.com>
2026-02-06 12:30:12 +0530

    Bit Manipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -266,3 +266,21 @@
 
 
 // Clear Range Of the Bits...
+
+public class JavaBasics {
+
+    public static int ClearRangeofBits(int n, int i, int j) {
+
+        int a = (~0) << (j + 1);
+        int b = (1 << i) - 1;
+
+        int bitmask = a | b;
+
+        return n & bitmask;
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println(ClearRangeofBits(15, 1, 2));
+    }
+}
