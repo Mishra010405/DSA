3c89a00cb7d9b91fb53e2c075fac0d24a5b54882
Shivam Mishra <mshivam2062@gmail.com>
2026-02-06 11:05:50 +0530

    Bit Manipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -194,3 +194,23 @@
 
 // Fast Exponential.....
 
+// public class JavaBasics {
+//     public static int FastExponential(int a, int n) {
+//         int ans = 1;
+
+//         while(n>0) {
+//             if((n & 1) != 0) { // Check LSB
+//                 ans = ans *a;
+//             }
+//             a = a*a;
+//             n= n>>1;
+//         }
+
+//         return ans;
+//     }
+
+//     public static void main(Strina args []) {
+//         System.out.println(FastExponential(3,5))
+
+//     }
+// }
