3fd7fe8972f218886f2d091e6cdbc178b2a91198
Shivam Mishra <mshivam2062@gmail.com>
2026-02-06 11:20:41 +0530

    Bit Manipulation

--- BitManipulation.java
+++ BitManipulation.java
@@ -216,12 +216,13 @@
 // }
 
 public class JavaBasics {
-    public static int getIthbit(int num , int i) {
-        int bitmask =1<<i;
-        return num | bitmask;
+    public static int ClearIthBit(int num , int i) {
+        int bitmask = ~(1<<i);
+        return num & bitmask;
 
     }
     public static void main(String args[]) {
-        System.out.println(getIthbit(10,2));
+        System.out.println(13,2);
+
     }
 }
