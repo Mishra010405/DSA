873f527db093d0ed3574040d8de45e2e2f45b6f9
Shivam Mishra <mshivam2062@gmail.com>
2026-02-07 15:02:06 +0530

    Bit Manipulation

--- OOPS.java
+++ OOPS.java
@@ -1,15 +1,27 @@
 public class OOPS {
-    public static void main(String args []) {
-        pen p1 = new pen(); // created a pen object called p1..
-        p1.setColor("Yellow");
-        System.out.println(p1.color); 
-    }
+    public static void main(String args[]) {
 
+        pen p1 = new pen(); // created a pen object called p1
+        p1.setColor("Yellow");
+        p1.setTip(5);
+
+        System.out.println("Pen Color: " + p1.color);
+        System.out.println("Pen Tip: " + p1.tip);
+
+        Student s1 = new Student();
+        s1.name = "Shivam";
+        s1.age = 20;
+        s1.calculatePercentage(80, 85, 90);
+
+        System.out.println("Student Name: " + s1.name);
+        System.out.println("Percentage: " + s1.percentage);
+    }
 }
 
 class pen {
-    String color,
-    int tip,
+
+    String color;
+    int tip;
 
     void setColor(String newColor) {
         color = newColor;
@@ -20,13 +32,13 @@ class pen {
     }
 }
 
-
 class Student {
+
     String name;
     int age;
     float percentage; // cgpa
 
-    void percentage(int phy , int chem , int math) {
-        percentage = (phy+chem+math)/3;
+    void calculatePercentage(int phy, int chem, int math) {
+        percentage = (phy + chem + math) / 3.0f;
     }
 }
