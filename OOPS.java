77df161a40606f042fc81f436c85220e114e7d54
Shivam Mishra <mshivam2062@gmail.com>
2026-02-07 15:23:23 +0530

    Bit Manipulation

--- OOPS.java
+++ OOPS.java
@@ -1,44 +1,68 @@
+// public class OOPS {
+//     public static void main(String args[]) {
+
+//         pen p1 = new pen(); // created a pen object called p1
+//         p1.setColor("Yellow");
+//         p1.setTip(5);
+
+//         System.out.println("Pen Color: " + p1.color);
+//         System.out.println("Pen Tip: " + p1.tip);
+
+//         Student s1 = new Student();
+//         s1.name = "Shivam";
+//         s1.age = 20;
+//         s1.calculatePercentage(80, 85, 90);
+
+//         System.out.println("Student Name: " + s1.name);
+//         System.out.println("Percentage: " + s1.percentage);
+
+
+//     }
+// }
+
+
+// class pen {
+
+//     String color;
+//     int tip;
+
+//     void setColor(String newColor) {
+//         color = newColor;
+//     }
+
+//     void setTip(int newTip) {
+//         tip = newTip;
+//     }
+// }
+
+// class Student {
+
+//     String name;
+//     int age;
+//     float percentage; // cgpa
+
+//     void calculatePercentage(int phy, int chem, int math) {
+//         percentage = (phy + chem + math) / 3.0f;
+//     }
+// }
+
+
+// Access Moidifiers.....
+
 public class OOPS {
-    public static void main(String args[]) {
+    public static void main(String ars[]) {
 
-        pen p1 = new pen(); // created a pen object called p1
-        p1.setColor("Yellow");
-        p1.setTip(5);
+        bankAccount myAcc = new bankAccount();
 
-        System.out.println("Pen Color: " + p1.color);
-        System.out.println("Pen Tip: " + p1.tip);
+        // Assign value to variable
+        myAcc.username = "Shivam Mishra";
 
-        Student s1 = new Student();
-        s1.name = "Shivam";
-        s1.age = 20;
-        s1.calculatePercentage(80, 85, 90);
-
-        System.out.println("Student Name: " + s1.name);
-        System.out.println("Percentage: " + s1.percentage);
+        System.out.println("The User name will be " + myAcc.username);
     }
 }
 
-class pen {
+class bankAccount {
 
-    String color;
-    int tip;
-
-    void setColor(String newColor) {
-        color = newColor;
-    }
-
-    void setTip(int newTip) {
-        tip = newTip;
-    }
-}
-
-class Student {
-
-    String name;
-    int age;
-    float percentage; // cgpa
-
-    void calculatePercentage(int phy, int chem, int math) {
-        percentage = (phy + chem + math) / 3.0f;
-    }
+    public String username;
+    private String password;
 }
