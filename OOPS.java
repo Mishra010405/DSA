af9d3e0a4b726ca33abc9d1cb2952afac8775a5a
Shivam Mishra <mshivam2062@gmail.com>
2026-02-10 22:28:33 +0530

    On Going Server And Client

--- OOPS.java
+++ OOPS.java
@@ -440,24 +440,24 @@
 public class OOPS {
     public static void main(String args[]) {
         Calculator clc = new Calculator();
-        System.out.println(clc.sum(1,2));
-        System.out.println(clc.sum(1.4,2.5));
-        System.out.println(clc.sum(1,2,4));
 
+        System.out.println(clc.sum(1, 2));          // int version
+        System.out.println(clc.sum(1.4, 2.5));      // double version
+        System.out.println(clc.sum(1, 2, 4));       // 3-int version
     }
 }
 
 class Calculator {
-    int sum(int a , int b) {
-        return a+b;
+
+    int sum(int a, int b) {
+        return a + b;
     }
 
-
-    int sum(float a, float b) {
-        return a+b;
+    double sum(double a, double b) {
+        return a + b;
     }
 
-    int sum(int a , int b , int c) {
-        return a+b+c;
+    int sum(int a, int b, int c) {
+        return a + b + c;
     }
 }
