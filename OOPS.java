cf17a7880f3e85ff86a90ee05933e6282e276e8e
Shivam Mishra <mshivam2062@gmail.com>
2026-02-10 22:27:24 +0530

    On Going Server And Client

--- OOPS.java
+++ OOPS.java
@@ -436,3 +436,28 @@
 // }
 
 // Calculator By Method Overloading......
+
+public class OOPS {
+    public static void main(String args[]) {
+        Calculator clc = new Calculator();
+        System.out.println(clc.sum(1,2));
+        System.out.println(clc.sum(1.4,2.5));
+        System.out.println(clc.sum(1,2,4));
+
+    }
+}
+
+class Calculator {
+    int sum(int a , int b) {
+        return a+b;
+    }
+
+
+    int sum(float a, float b) {
+        return a+b;
+    }
+
+    int sum(int a , int b , int c) {
+        return a+b+c;
+    }
+}
