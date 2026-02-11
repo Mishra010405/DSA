94302615865eb17990d87abeca31f3b2d925cfb1
Shivam Mishra <mshivam2062@gmail.com>
2026-02-11 23:06:30 +0530

    On Going Server And Client

--- basic.java
+++ basic.java
@@ -339,22 +339,22 @@
 
 // Geomatric progression ....
 
-// import java.util.Scanner;
+import java.util.Scanner;
 
-// public class JavaBasics {
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
+public class JavaBasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         
-//         int a = sc.nextInt();   
-//         int r = sc.nextInt();   
-//         int n = sc.nextInt();   
+        int a = sc.nextInt();   
+        int r = sc.nextInt();   
+        int n = sc.nextInt();   
 
-//         int term = a;
+        int term = a;
 
-//         for (int i = 1; i <= n; i++) {
-//             System.out.print(term + " ");
-//             term = term * r;
-//         }
-//     }
-// }
+        for (int i = 1; i <= n; i++) {
+            System.out.print(term + " ");
+            term = term * r;
+        }
+    }
+}
