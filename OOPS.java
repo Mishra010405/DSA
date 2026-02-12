f7e35ad98f811051f568768a1b379078951c6874
Shivam Mishra <mshivam2062@gmail.com>
2026-02-12 23:01:46 +0530

    On Going Server And Client

--- OOPS.java
+++ OOPS.java
@@ -561,3 +561,15 @@
 // ✔ Correct Answer:
 
 // C. 13245
+
+// public class ExceptionDemo {
+//     public static void main(String[] args) {
+//         try {
+//             int a = 10 / 0;
+//         } catch (ArithmeticException e) {
+//             System.out.println("Cannot divide by zero");
+//         } finally {
+//             System.out.println("Program finished");
+//         }
+//     }
+// }
