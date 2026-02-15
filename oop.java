5017537f17fa2786c5ca361746a7fb387989b445
Shivam Mishra <mshivam2062@gmail.com>
2026-02-15 09:58:52 +0530

    Obejct Orinted Programing

--- oop.java
+++ oop.java
@@ -1,26 +1,38 @@
-// public class JavaBasics {
-//     public static void main(String args[]) {
-//         Pen p1 = new Pen();
+public class JavaBasics {
+    public static void main(String args[]) {
+        Pen p1 = new Pen();
 
-//         p1.setColor("Blue");
-//         System.out.println(p1.color);
-//         p1.setTips(24);
-//         System.out.println(p1.tip);
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+        p1.setTips(24);
+        System.out.println(p1.tip);
 
-//     }
-// }
+        bankAccount p2 = new bankAccount();
+        p2.username = "Shivam";
+        System.out.println(p2.username);
 
-// class Pen {
-//     String color;
-//     int tip;
 
-//     void setColor(String newColor) {
-//         color = newColor;
-//     }
+    }
+}
 
-//     void setTips(int newTip) {
-//         tip = newTip;
-//     }
-// }
+class bankAccount {
+    public String username;
+    private String password;
+    
+
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTips(int newTip) {
+        tip = newTip;
+    }
+}
 
 
