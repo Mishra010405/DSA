b5727cd788d54f43b1164e1b547020565081e10a
Shivam Mishra <mshivam2062@gmail.com>
2026-02-15 10:43:23 +0530

    Obejct Orinted Programing

--- oop.java
+++ oop.java
@@ -1,58 +1,62 @@
-public class JavaBasics {
-    public static void main(String args[]) {
+// public class JavaBasics {
+//     public static void main(String args[]) {
 
-        Pen p1 = new Pen();
+//         Pen p1 = new Pen();
 
-        p1.setColor("Blue");
-        System.out.println(p1.getColor());
+//         p1.setColor("Blue");
+//         System.out.println(p1.getColor());
 
-        p1.setTips(24);
-        System.out.println(p1.getTip());
-
-
-        BankAccount p2 = new BankAccount();
-
-        p2.setUsername("Shivam");
-        System.out.println(p2.getUsername());
-    }
-}
+//         p1.setTips(24);
+//         System.out.println(p1.getTip());
 
 
-// BankAccount Class
-class BankAccount {
-    private String username;
-    private String password;
+//         BankAccount p2 = new BankAccount();
 
-    // Setter
-    void setUsername(String name) {
-        this.username = name;
-    }
-
-    // Getter
-    String getUsername() {
-        return this.username;
-    }
-}
+//         p2.setUsername("Shivam");
+//         System.out.println(p2.getUsername());
+//     }
+// }
 
 
-// Pen Class
-class Pen {
-    private String color;
-    private int tip;
+// // BankAccount Class
+// class BankAccount {
+//     private String username;
+//     private String password;
 
-    String getColor() {
-        return this.color;
-    }
+//     // Setter
+//     void setUsername(String name) {
+//         this.username = name;
+//     }
 
-    int getTip() {
-        return this.tip;
-    }
+//     // Getter
+//     String getUsername() {
+//         return this.username;
+//     }
+// }
 
-    void setColor(String newColor) {
-        this.color = newColor;
-    }
 
-    void setTips(int newTip) {
-        this.tip = newTip;
-    }
-}
+// // Pen Class
+// class Pen {
+//     private String color;
+//     private int tip;
+
+//     String getColor() {
+//         return this.color;
+//     }
+
+//     int getTip() {
+//         return this.tip;
+//     }
+
+//     void setColor(String newColor) {
+//         this.color = newColor;
+//     }
+
+//     void setTips(int newTip) {
+//         this.tip = newTip;
+//     }
+// }
+
+
+// Constructor.....
+
