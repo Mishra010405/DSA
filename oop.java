865ad4f8083610bd7b598835d190c7cac131c929
Shivam Mishra <mshivam2062@gmail.com>
2026-02-15 09:53:08 +0530

    On Going Server And Client

--- oop.java
+++ oop.java
@@ -1,24 +1,26 @@
-public class JavaBasics {
-    public static void main(String args[]) {
-        Pen p1 = new Pen();
+// public class JavaBasics {
+//     public static void main(String args[]) {
+//         Pen p1 = new Pen();
 
-        p1.setColor("Blue");
-        System.out.println(p1.color);
-        p1.setTips(24);
-        System.out.println(p1.tip);
+//         p1.setColor("Blue");
+//         System.out.println(p1.color);
+//         p1.setTips(24);
+//         System.out.println(p1.tip);
 
-    }
-}
+//     }
+// }
 
-class Pen {
-    String color;
-    int tip;
+// class Pen {
+//     String color;
+//     int tip;
 
-    void setColor(String newColor) {
-        color = newColor;
-    }
+//     void setColor(String newColor) {
+//         color = newColor;
+//     }
 
-    void setTips(int newTip) {
-        tip = newTip;
-    }
-}
+//     void setTips(int newTip) {
+//         tip = newTip;
+//     }
+// }
+
+
