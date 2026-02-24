14d8683e527b663c3b620704970e9cc542310d11
Shivam Mishra <mshivam2062@gmail.com>
2026-02-24 20:46:44 +0530

    Recursion

--- resoursion.java
+++ resoursion.java
@@ -1,15 +1,72 @@
-public class RecursionBsics {
-    public static void printdec(int n) {
-        if(n==1) {
-            System.out.println(n);
-            return;
-        }
-        System.out.println(n+" ");
-        printdec(n-1);
-    }
-    public static void main(String args[]) {
-        int n=10;
-        printdec(10);
+// Print the number from 10 to n...
 
-    }
-}
+// public class recursionBasics{
+//     public static void printDec(int n) {
+//         if(n==1) {
+//             System.out.println(n+" ");
+//             return;
+
+//         }
+//         System.out.println(n+" ");
+//         printDec(n-1);
+//     }
+//     public static void main(String args[]) {
+//         int n=10;
+//         printDec(n);
+
+//     }
+// }
+
+
+
+// Print the number in increasing order...
+
+// public class recursionBasics {
+
+//     public static void IncreaseNum(int num) {
+
+//         if(num == 10) {
+//             System.out.println(num + " ");
+//             return;
+//         }
+
+//         System.out.println(num + " ");
+//         IncreaseNum(num + 1);
+//     }
+
+//     public static void main(String args[]) {
+
+//         int num = 1;
+//         IncreaseNum(num);
+
+//     }
+// }
+
+
+// Factorial on the number....
+
+// public class recursionBasics {
+
+//     public static int factorailofnum(int num) {
+
+//         if(num == 0) {
+//             return 1;  
+//         }
+
+//         int fnm1 = factorailofnum(num - 1);
+//         int fn = num * fnm1;
+
+//         return fn;
+//     }
+
+//     public static void main(String args[]) {
+
+//         int num = 5;
+
+//         int result = factorailofnum(num);
+
+//         System.out.println(result);
+
+//     }
+// }
+
